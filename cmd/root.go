de3e7113a08653f0dca5fc4fdefe1c9f3e1b7ebf
DragonHeaven <happyfamilylife11@gmail.com>
2020-06-15 21:57:26 +0200

    ReadConfig

--- cmd/root.go
+++ cmd/root.go
@@ -18,13 +18,15 @@ package cmd
 import (
 	//"context"
 	"fmt"
+	"github.com/dragonheaven/pegnet-stakingd/exit"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/dragonheaven/pegnet-stakingd/config"
-	"github.com/dragonheaven/pegnet-stakingd/exit"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -81,14 +83,57 @@ func always(cmd *cobra.Command, args []string) {
 	// Also init some defaults
 	viper.SetDefault(config.DBlockSyncRetryPeriod, time.Second*5)
 	viper.SetDefault(config.SqliteDBPath, "$HOME/.pegnet-stakingd/mainnet/sql.db")
+
+	// Catch ctl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		<-signalChan
+		log.Info("Gracefully closing")
+		exit.GlobalExitHandler.Close()
+
+		log.Info("closing application")
+		// If something is hanging, we have to kill it
+		os.Exit(0)
+	}()
 }
 
 // ReadConfig can be put as a PreRun for a command that uses the config file
 func ReadConfig(cmd *cobra.Command, args []string) {
 	log.Infof("ReadConfig is called")
+
+	err := viper.ReadInConfig()
+	// If no config is found, we will attempt to make one
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		log.WithError(err).Fatal("config file not found")
+	} else if err != nil {
+		log.WithError(err).Fatal("failed to load config")
+	}
+
+	// Indicate which config was used
+	log.Infof("Using config from %s", viper.ConfigFileUsed())
+
+	initLogger()
 }
 
 func init() {
 	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.newApp.yaml)")
 	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initLogger() {
+	switch strings.ToLower(viper.GetString(config.LoggingLevel)) {
+	case "trace":
+		log.SetLevel(log.TraceLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	}
 }
